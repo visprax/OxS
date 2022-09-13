df840b8a57aa3cdd5edccefba601c22e4caee84c
Saeid Aliei <saeidaliei2019@gmail.com>
2022-09-13 21:57:05 +0430

    print hello and loop infinitely

--- bootsector/bootsector.asm
+++ bootsector/bootsector.asm
@@ -1,11 +1,26 @@
 ;
-; A simple boot sector program that loops infinitely.
+; A simple boot sector program that prints "Hello" and loops infinitely.
 ; A boot sector is identified by the BIOS as the region containing 
 ; 512 bytes with the last two bytes set to the two magic numbers 
 ; 0xAA and 0x55 which tells the BIOS this is a boot sector program, 
 ; and not some random data, the rest of the 510 bytes we set it to zero.
+; For printing we use CPU interrupts and specifically 0x10 which causes 
+; screen related interrupt routine service (ISR) to be invoked.
 ;
 
+; scrolling teletype BIOS routine
+mov ah, 0x0e
+
+mov al, "H"
+int 0x10
+mov al, "e"
+int 0x10
+mov al, "l"
+int 0x10
+mov al, "l"
+int 0x10
+mov al, "o"
+int 0x10
 
 ; $ is the position at the beginning of the line containing the expression.
 jmp $
